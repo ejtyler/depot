49ca1201a3e19009a237f9a1e4f4c9d411b22ca7
Eric Tyler <tyler.ericj@gmail.com>
2018-10-01 21:49:14 -0400

    remove delete products and line items to prevent errors in db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Line_Item.delete_all
-Product.delete_all
+# Line_Item.delete_all
+# Product.delete_all
 # ...
 Product.create!(title: 'Seven Mobile Apps in Seven Weeks',
 	description:
